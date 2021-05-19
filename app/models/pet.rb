b57b02402fd728fe0fa4f91a5a4c39286a5ab0cf
claire gautier <claire.gautier@edhec.com>
2021-05-19 08:50:34 +0200

    finished livecode

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,12 @@
 class Pet < ApplicationRecord
   SPECIES = ['dog', 'cat', 'dolphin', 'turtle', 'bunny']
   validates :species, inclusion: { in: SPECIES }
+  validates :name, presence: true
+
+
+  def found_days_ago
+    days = Date.today.day - self.found_on.day
+    "This pet was found #{days} day(s) ago"
+  end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pet.create(
+  name: "Momo",
+  address: 'Stockholm',
+  found_on: Date.today - 2,
+  species: "turtle")
+
+Pet.create(
+  name: "Fluffy",
+  address: 'Paris',
+  found_on: Date.today - 3,
+  species: "dog")
+
+Pet.create(
+  name: "Flipper",
+  address: 'London',
+  found_on: Date.today - 5,
+  species: "dolphin")
+
+Pet.create(
+  name: "Garfield",
+  address: 'Barcelona',
+  found_on: Date.today - 1,
+  species: "cat")
